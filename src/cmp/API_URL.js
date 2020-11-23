c146a6ad05cc5f9f8ce42b8285724e27cd1aa308
Lavdeep Singh <lovedeepsingh123.lk@gmail.com>
2020-11-23 17:06:05 +0530

    link added

--- src/cmp/API_URL.js
+++ src/cmp/API_URL.js
@@ -1,4 +1,6 @@
 
 // const url='http://localhost:3001'
-const url='http://192.168.43.100:3001'
+//const url='http://192.168.43.100:3001'
+const url='https://hot-earwig-15.loca.lt'
+
 export default url
