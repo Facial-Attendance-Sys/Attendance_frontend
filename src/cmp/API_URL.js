5323c7ba079e22a676c5e1b9092a877a8e88963c
Lavdeep Singh <lovedeepsingh123.lk@gmail.com>
2020-11-23 18:01:12 +0530

    link added

--- src/cmp/API_URL.js
+++ src/cmp/API_URL.js
@@ -1,6 +1,6 @@
 
 // const url='http://localhost:3001'
 //const url='http://192.168.43.100:3001'
-const url='https://hot-earwig-15.loca.lt'
+const url='https://343032f08cc9.ngrok.io'
 
 export default url
