53988d186388abc8670238bc3763b795eca9765b
Lavdeep Singh <lovedeepsingh123.lk@gmail.com>
2020-12-13 21:26:50 +0530

    url changed

--- src/cmp/API_URL.js
+++ src/cmp/API_URL.js
@@ -1,6 +1,6 @@
 
 // const url='http://localhost:3001'
 // const url='http://192.168.43.100:3001'
-const url='https://a5434f99f07d.ngrok.io'
+const url='https://95226c4bfac1.ngrok.io'
 
 export default url
